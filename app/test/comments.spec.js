f0588c29ef9df1af9257742ce8060d38c807185d
Jessie Leung <jleung@thoughtworks.com>
2016-05-30 14:06:48 +1000

    First (redundant) unit test

---
+++ app/test/comments.spec.js
@@ -0,0 +1,9 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+describe("First test", function() {
+  it("First test", function() {
+    var test = true;
+    expect(test).to.equal(true);
+  });
+});
