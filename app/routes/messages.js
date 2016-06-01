6284d1df8862aff87acde4aea1b6bbae15dc46bc
Jessie Leung <jleung@thoughtworks.com>
2016-06-01 11:01:16 +1000

    Api tests

--- app/routes/messages.js
+++ app/routes/messages.js
@@ -1,8 +1,28 @@
 var express = require('express');
+var models  = require('../models');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.status(200).json({ title: 'Messages' });
+  models.Message.findAll().then(function(messages) {
+    res.status(200).json(messages);
+  });
+});
+
+router.post('/', function(req, res, next) {
+  if (req.body.content === undefined) {
+    res.status(400).json("Invalid message content");
+    return;
+  }
+  var message = models.Message.build({
+    content: req.body.content
+  });
+
+  message.save()
+    .then(function(err) {
+      if (err) {
+        res.json(err);
+      }
+  });
 });
 
 module.exports = router;
--- test/messages.spec.js
+++ test/messages.spec.js
@@ -1,12 +1,32 @@
 var chai = require('chai');
 var expect = chai.expect;
-var supertest = require("supertest");
-var api = supertest.agent('http://localhost:3000');
+var request = require("supertest");
+var app = require("../app/app");
 
 describe('Messages', function() {
-  it('should return 200 response on /messages GET', function() {
-    api.get('/messages')
+  it('should return 200 response on /messages GET', function(done) {
+    request(app).get('/messages')
       .set('Accept', 'application/json')
-      .expect(200);
+      .expect(200, done);
+  });
+
+  it('should return 200 response on /messages POST', function(done) {
+    var message = {
+      "content": "This is a test message"
+    }
+    request(app).post('/messages')
+      .set('Accept', 'application/json')
+      .send(message)
+      .expect(200, done);
+  });
+
+  it('should return 400 response on /messages POST given no content', function(done) {
+    var message = {
+      "nocontent": "message"
+    }
+    request(app).post('/messages')
+      .set('Accept', 'application/json')
+      .send(message)
+      .expect(400, done);
   });
 });
