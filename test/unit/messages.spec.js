d9ce7992c44633b13b01f44424a20da6447b523d
Jessie Leung <jleung@thoughtworks.com>
2016-09-13 17:32:02 +1000

    Random assertions for the demo

--- test/unit/messages.spec.js
+++ test/unit/messages.spec.js
@@ -2,7 +2,8 @@ var chai = require('chai');
 var expect = chai.expect;
 
 describe('Messages', function() {
-  it('should return true', function() {
+  it('should do some random assertions for the demo', function() {
     expect(true).to.equal(true);
+    expect(1 + 1).to.equal(2);
   });
 });
